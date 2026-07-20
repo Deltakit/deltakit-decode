3c5522b20f0e347ce2e0f33ae8202a44d912db2f
Roland Guichard <roland.guichard@riverlane.com>
2026-07-20 18:30:48 +0100

    Fix import and path.

--- tests/analysis/test_decoder_manager.py
+++ tests/analysis/test_decoder_manager.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 import pathos
 import pytest
-import stim
+import deltakit_stim as stim
 from deltakit_core.decoding_graphs import dem_to_decoding_graph_and_logicals
 
 from deltakit_decode import PyMatchingDecoder
--- tests/unit/utils/test_derivation_tools.py
+++ tests/unit/utils/test_derivation_tools.py
@@ -17,7 +17,7 @@ from deltakit_decode.utils._derivation_tools import (
 from deltakit_decode.utils._graph_circuit_helpers import parse_stim_circuit
 
 REFERENCE_DATA_DIR = (
-    Path(__file__).parent.parent.parent.parent.parent / "tests" / "reference_data"
+    Path(__file__).resolve().parents[2] / "reference_data"
 )
 
 
